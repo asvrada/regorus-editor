68ada9c5375632d05b55eb440d9d2882e0bc8909
Zijie Wu <noojeff@gmail.com>
2024-02-06 11:00:23 -0800

    Add regorus

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,57 @@
+import { useEffect } from "react";
+
+import init, { Engine } from "regorus";
+
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    init().then(() => {
+      let engine = new Engine();
+
+      engine.add_policy(
+        "hello.rego",
+        `
+    package test
+
+    message = concat(", ", [input.message, data.message])
+`,
+      );
+
+      engine.add_data(`{
+  "message": "World!"
+}`);
+
+      engine.set_input(`{
+    "message": "Hello"
+  }`);
+
+      let results = engine.eval_query("data.test.message");
+
+      console.log(results);
+    });
+  }, []);
+
   return (
-    <div>Hello, World!</div>
+    <div>
+      {/* Header for things like Evaluate/Format/Publish buttons */}
+      <div></div>
+
+      {/* Main Editor body */}
+      <div>
+        {/* Left window - Rego policy data */}
+        <div></div>
+
+        {/* Right Column for a list of windows */}
+        <div>
+          {/* First window - Input */}
+          <div></div>
+
+          {/* Second window - Output */}
+          <div></div>
+        </div>
+      </div>
+    </div>
   );
 }
 
