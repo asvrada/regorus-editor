b7d6a816e82c081bdae0787e39821e9b0fa1f335
Zijie Wu <noojeff@gmail.com>
2024-02-06 16:26:07 -0800

    Format everything

--- src/App.jsx
+++ src/App.jsx
@@ -116,7 +116,7 @@ const EXAMPLES = {
 
 /**
  * Load examples from internet
- * @param {{policy: string, input: string, data: string}}  
+ * @param {{policy: string, input: string, data: string}}
  * @returns {Promise<{policy: string, input: string, data: string}>} Content of given 3 files
  */
 async function loadExample({ policy, input, data }) {
@@ -188,126 +188,124 @@ function App() {
   }
 
   return (
-    <>
-      <div className="flex h-screen flex-col bg-slate-300">
-        {/* Header for things like Evaluate/Format/Publish buttons */}
-        <header className="my-2 flex flex-none justify-between">
-          <h2 className="ml-6 text-2xl font-bold">Regorus Playground</h2>
+    <div className="flex h-screen flex-col bg-slate-300">
+      {/* Header for things like Evaluate/Format/Publish buttons */}
+      <header className="my-2 flex flex-none justify-between">
+        <h2 className="ml-6 text-2xl font-bold">Regorus Playground</h2>
 
-          <div className="mr-6">
-            <button
-              className="mx-4 rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
-              onClick={async () => {
-                const { policy, input, data } = await loadExample(EXAMPLES.example1);
+        <div className="mr-6">
+          <button
+            className="mx-4 rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+            onClick={async () => {
+              const { policy, input, data } = await loadExample(
+                EXAMPLES.example1,
+              );
 
-                editorPolicyRef.current.setValue(policy);
-                editorInputRef.current.setValue(input);
-                editorDataRef.current.setValue(data);
+              editorPolicyRef.current.setValue(policy);
+              editorInputRef.current.setValue(input);
+              editorDataRef.current.setValue(data);
+            }}
+          >
+            Load Example 1
+          </button>
+
+          <button
+            className="mx-4 rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+            onClick={onClickEvaluate}
+          >
+            Evaluate
+          </button>
+
+          <a href="https://github.com/microsoft/regorus">GitHub Icon</a>
+        </div>
+      </header>
+
+      {/* Main Editor body */}
+      <div className="flex-auto grow">
+        <div className="flex h-full flex-row space-x-4">
+          {/* Left window - Rego policy */}
+          <div className="flex-auto">
+            <Editor
+              defaultLanguage="Rego"
+              defaultValue={defaultExample.policy}
+              options={{
+                minimap: {
+                  enabled: false,
+                },
               }}
-            >
-              Load Example 1
-            </button>
-
-            <button
-              className="mx-4 rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
-              onClick={onClickEvaluate}
-            >
-              Evaluate
-            </button>
-
-            <a href="https://github.com/microsoft/regorus">GitHub Icon</a>
+              beforeMount={(monaco) => {
+                monaco.languages.register({ id: "Rego" });
+                monaco.languages.setMonarchTokensProvider(
+                  "Rego",
+                  REGO_LANGUAGE,
+                );
+              }}
+              onMount={(editor) => {
+                editorPolicyRef.current = editor;
+              }}
+            />
           </div>
-        </header>
 
-        {/* Main Editor body */}
-        <div className="flex-auto grow">
-          <div className="flex h-full flex-row space-x-4">
-            {/* Left window - Rego policy */}
-            <div className="flex-auto">
+          {/* Right Column for a list of windows */}
+          <div className="flex flex-auto flex-col space-y-1">
+            {/* First window - Input */}
+            <div className="flex flex-1 flex-col">
+              <div className="flex-1 text-sm font-bold">Input Editor</div>
               <Editor
-                defaultLanguage="Rego"
-                defaultValue={defaultExample.policy}
+                className="flex-1"
+                defaultLanguage="json"
+                defaultValue={defaultExample.input}
                 options={{
                   minimap: {
-                    enabled: false
-                  }
-                }}
-                beforeMount={(monaco) => {
-                  monaco.languages.register({ id: "Rego" });
-                  monaco.languages.setMonarchTokensProvider(
-                    "Rego",
-                    REGO_LANGUAGE,
-                  );
+                    enabled: false,
+                  },
                 }}
                 onMount={(editor) => {
-                  editorPolicyRef.current = editor;
+                  editorInputRef.current = editor;
                 }}
               />
             </div>
 
-            {/* Right Column for a list of windows */}
-            <div className="flex flex-auto flex-col space-y-1">
-              {/* First window - Input */}
-              <div className="flex flex-1 flex-col">
-                <div className="flex-1 text-sm font-bold">Input Editor</div>
-                <Editor
-                  className="flex-1"
-                  defaultLanguage="json"
-                  defaultValue={defaultExample.input}
-                  options={{
-                    minimap: {
-                      enabled: false
-                    }
-                  }}
-                  onMount={(editor) => {
-                    editorInputRef.current = editor;
-                  }}
-                />
-              </div>
+            {/* Second window - Data */}
+            <div className="flex flex-1 flex-col">
+              <div className="flex-1 text-sm font-bold">Data Editor</div>
+              <Editor
+                className="flex-1"
+                defaultLanguage="json"
+                defaultValue={defaultExample.data}
+                options={{
+                  minimap: {
+                    enabled: false,
+                  },
+                }}
+                onMount={(editor) => {
+                  editorDataRef.current = editor;
+                }}
+              />
+            </div>
 
-              {/* Second window - Data */}
-              <div className="flex flex-1 flex-col">
-                <div className="flex-1 text-sm font-bold">Data Editor</div>
-                <Editor
-                  className="flex-1"
-                  defaultLanguage="json"
-                  defaultValue={defaultExample.data}
-                  options={{
-                    minimap: {
-                      enabled: false
-                    }
-                  }}
-                  onMount={(editor) => {
-                    editorDataRef.current = editor;
-                  }}
-                />
-              </div>
-
-              {/* Third window - Output */}
-              <div className="flex flex-1 flex-col">
-                <div className="flex-1 text-sm font-bold">
-                  Output - {result}
-                </div>
-                <Editor
-                  className="flex-1"
-                  defaultLanguage="json"
-                  defaultValue="// Output goes here"
-                  options={{
-                    readOnly: true,
-                    minimap: {
-                      enabled: false,
-                    },
-                  }}
-                  onMount={(editor) => {
-                    editorOutputRef.current = editor;
-                  }}
-                />
-              </div>
+            {/* Third window - Output */}
+            <div className="flex flex-1 flex-col">
+              <div className="flex-1 text-sm font-bold">Output - {result}</div>
+              <Editor
+                className="flex-1"
+                defaultLanguage="json"
+                defaultValue="// Output goes here"
+                options={{
+                  readOnly: true,
+                  minimap: {
+                    enabled: false,
+                  },
+                }}
+                onMount={(editor) => {
+                  editorOutputRef.current = editor;
+                }}
+              />
             </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
